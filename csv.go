12047ebc03908a69c50efc4606eb5bb0ad0585d6
Teon Lucas <tlucas@newrelic.com>
2024-09-12 16:08:10 -0700

    Add getDashboardDetails to pull widgets

--- csv.go
+++ csv.go
@@ -23,19 +23,33 @@ func (data *LocalData) writeCSV() {
 		"name",
 		"permalink",
 		"createdBy",
+		"chartId",
+		"chartName",
+		"nrqlAccountId",
+		"nrqlQuery",
 	})
 	for _, dashboard := range data.DashboardMap {
-		rows = append(rows, []string{
-			fmt.Sprintf("%d", dashboard.AccountId),
-			dashboard.Guid,
-			dashboard.Name,
-			dashboard.Permalink,
-			dashboard.CreatedBy,
-		})
+		//log.Printf("Found Dashboard %s, page=%v", dashboard.Guid, dashboard.IsPage)
+		for _, widgetId := range dashboard.WidgetIds {
+			widget := dashboard.WidgetMap[widgetId]
+			for _, query := range widget.NrqlQueries {
+				rows = append(rows, []string{
+					fmt.Sprintf("%d", dashboard.AccountId),
+					dashboard.Guid,
+					dashboard.Name,
+					dashboard.Permalink,
+					dashboard.CreatedBy,
+					fmt.Sprintf("%d", widgetId),
+					widget.Title,
+					fmt.Sprintf("%d", query.AccountId),
+					query.Query,
+				})
+			}
+		}
 	}
 
 	// Write CSV
-	log.Printf("Writing csv %s", outputCSV)
+	log.Printf("Writing csv %s with %d entries", outputCSV, len(rows)-1)
 	w := csv.NewWriter(f)
 	err = w.WriteAll(rows)
 	if err != nil {
--- graphql.go
+++ graphql.go
@@ -6,39 +6,43 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sort"
+	"strconv"
 	"strings"
 	"time"
 )
 
 const (
 	GraphQlEndpoint = "https://api.newrelic.com/graphql"
-	GrQl_Parallel   = 8
-	DashboardQuery  = `query EntitySearchQuery($cursor: String) {actor {entitySearch(query: "domain = 'VIZ' AND type = 'DASHBOARD' AND accountId = %d", options: {tagFilter: ["createdBy"]}) {results(cursor: $cursor) {entities {guid accountId name permalink tags {key values}} nextCursor}}}}`
-	DetailQuery     = `query getConditionDetail($accountId: Int!, $conditionId: ID!) {actor {account(id: $accountId) {alerts {nrqlCondition(id: $conditionId) {nrql {query} name id}}}}}`
+	GrQl_Parallel   = 10
+	DashboardQuery  = `query EntitySearchQuery($cursor: String) {actor {entitySearch(query: "domain = 'VIZ' AND type = 'DASHBOARD' AND accountId = %d", options: {tagFilter: ["createdBy","isDashboardPage"]}) {results(cursor: $cursor) {entities {guid accountId name permalink tags {key values}} nextCursor}}}}`
+	DetailQuery     = `query getDashboard($guid: EntityGuid!) {actor {entity(guid: $guid) {... on DashboardEntity {name pages {name widgets {configuration {area {nrqlQueries {accountId query}} bar {nrqlQueries {accountId query}} billboard {nrqlQueries {accountId query}} line {nrqlQueries {accountId query}} markdown {text} pie {nrqlQueries {accountId query}} table {nrqlQueries {accountId query}}} id title} guid}}}}}`
 )
 
-type Dashboard struct {
-	AccountId int    `json:"accountId"`
-	Guid      string `json:"guid"`
-	Name      string `json:"name"`
-	Permalink string `json:"permalink"`
-	CreatedBy string `json:"createdBy"`
-	Query     string
-}
-
-// GraphQl request and result formats
+// GraphQl request
 type GraphQlPayload struct {
 	Query     string `json:"query"`
 	Variables struct {
-		AccountId   int    `json:"accountId,omitempty"`
-		ConditionId string `json:"conditionId,omitempty"`
-		Cursor      string `json:"cursor,omitempty"`
+		AccountId int    `json:"accountId,omitempty"`
+		Guid      string `json:"guid,omitempty"`
+		Cursor    string `json:"cursor,omitempty"`
 	} `json:"variables"`
 }
+
+// GraphQl result
 type GraphQlResult struct {
 	Errors []Error `json:"errors"`
 	Data   struct {
 		Actor struct {
+			Entity struct {
+				Guid  string `json:"guid"`
+				Name  string `json:"name"`
+				Pages []struct {
+					Guid       string      `json:"guid"`
+					Name       string      `json:"name"`
+					RawWidgets []RawWidget `json:"widgets"`
+				}
+			} `json:"entity"`
 			EntitySearch struct {
 				Results struct {
 					Entities   []Entity    `json:"entities"`
@@ -47,6 +51,37 @@ type GraphQlResult struct {
 			} `json:"entitySearch"`
 		} `json:"actor"`
 	} `json:"data"`
+}
+type RawWidget struct {
+	Configuration struct {
+		Area struct {
+			NrqlQueries []NrqlQuery `json:"nrqlQueries"`
+		} `json:"area"`
+		Bar struct {
+			NrqlQueries []NrqlQuery `json:"nrqlQueries"`
+		} `json:"bar"`
+		Billboard struct {
+			NrqlQueries []NrqlQuery `json:"nrqlQueries"`
+		} `json:"billboard"`
+		Line struct {
+			NrqlQueries []NrqlQuery `json:"nrqlQueries"`
+		} `json:"line"`
+		Markdown struct {
+			NrqlQueries []NrqlQuery `json:"nrqlQueries"`
+		} `json:"markdown"`
+		Pie struct {
+			NrqlQueries []NrqlQuery `json:"nrqlQueries"`
+		} `json:"pie"`
+		Table struct {
+			NrqlQueries []NrqlQuery `json:"nrqlQueries"`
+		} `json:"table"`
+	} `json:"configuration"`
+	Id    string `json:"id"`
+	Title string `json:"title"`
+}
+type NrqlQuery struct {
+	AccountId int    `json:"accountId"`
+	Query     string `json:"query"`
 }
 type Entity struct {
 	AccountId int    `json:"accountId"`
@@ -60,6 +95,24 @@ type Entity struct {
 }
 type Error struct {
 	Message string `json:"message"`
+}
+
+// Cleaned up structures
+type Dashboard struct {
+	AccountId int    `json:"accountId"`
+	Guid      string `json:"guid"`
+	Name      string `json:"name"`
+	Permalink string `json:"permalink"`
+	CreatedBy string `json:"createdBy"`
+	IsPage    bool   `json:"isPage"`
+	WidgetMap map[int]Widget
+	WidgetIds []int
+}
+type Widget struct {
+	Id          int         `json:"id"`
+	Title       string      `json:"title"`
+	Guid        string      `json:"guid"`
+	NrqlQueries []NrqlQuery `json:"nrqlQueries"`
 }
 
 // Make API request with error retry
@@ -108,10 +161,20 @@ func parseEntity(entity Entity) (dashboard Dashboard) {
 	dashboard.Name = entity.Name
 	dashboard.Permalink = entity.Permalink
 
-	if len(entity.Tags) == 1 {
-		if len(entity.Tags[0].Values) == 1 {
-			dashboard.CreatedBy = entity.Tags[0].Values[0]
+	if len(entity.Tags) == 2 {
+		for _, tag := range entity.Tags {
+			if tag.Key == "createdBy" {
+				if len(tag.Values) == 1 {
+					dashboard.CreatedBy = tag.Values[0]
+				}
+			} else {
+				if len(tag.Values) == 1 {
+					dashboard.IsPage = tag.Values[0] == "true"
+				}
+			}
 		}
+	} else {
+		log.Printf("Expected 2 tags, found %d on dashboard %s", len(entity.Tags), entity.Name)
 	}
 	return
 }
@@ -120,7 +183,6 @@ func (data *LocalData) getDashboards() {
 	var gQuery GraphQlPayload
 	var j []byte
 	var err error
-	var conditionCount int
 
 	// Get conditions, story in Policy map by guid
 	gQuery.Query = fmt.Sprintf(DashboardQuery, data.AccountId)
@@ -146,7 +208,15 @@ func (data *LocalData) getDashboards() {
 
 		// store dashboards
 		for _, entity := range dashboardSearch.Entities {
-			data.DashboardMap[entity.Guid] = parseEntity(entity)
+			dashboard := parseEntity(entity)
+			dashboard.WidgetMap = make(map[int]Widget)
+			if dashboard.IsPage {
+				// if it is a page, save it in the map
+				data.DashboardMap[entity.Guid] = dashboard
+			} else {
+				// otherwise it is just a guid for the details query
+				data.ParentGuids = append(data.ParentGuids, entity.Guid)
+			}
 		}
 		if dashboardSearch.NextCursor == nil {
 			break
@@ -154,7 +224,125 @@ func (data *LocalData) getDashboards() {
 		// get next page of results
 		gQuery.Variables.Cursor = fmt.Sprintf("%s", dashboardSearch.NextCursor)
 	}
-	log.Printf("Found %d conditions", conditionCount)
+	log.Printf("Found %d dashboards, %d total pages", len(data.ParentGuids), len(data.DashboardMap))
+}
+
+func parseWidget(raw RawWidget) (widget Widget) {
+	var err error
+	widget.Id, err = strconv.Atoi(raw.Id)
+	if err != nil {
+		log.Printf("Error parsing widget id: %v", err)
+	}
+	widget.Title = raw.Title
+
+	// Streamline structure
+	if len(raw.Configuration.Area.NrqlQueries) > 0 {
+		widget.NrqlQueries = raw.Configuration.Area.NrqlQueries
+	} else if len(raw.Configuration.Bar.NrqlQueries) > 0 {
+		widget.NrqlQueries = raw.Configuration.Bar.NrqlQueries
+	} else if len(raw.Configuration.Billboard.NrqlQueries) > 0 {
+		widget.NrqlQueries = raw.Configuration.Billboard.NrqlQueries
+	} else if len(raw.Configuration.Line.NrqlQueries) > 0 {
+		widget.NrqlQueries = raw.Configuration.Line.NrqlQueries
+	} else if len(raw.Configuration.Pie.NrqlQueries) > 0 {
+		widget.NrqlQueries = raw.Configuration.Pie.NrqlQueries
+	} else if len(raw.Configuration.Table.NrqlQueries) > 0 {
+		widget.NrqlQueries = raw.Configuration.Table.NrqlQueries
+	}
+	return
+}
+
+func (data *LocalData) getDashboardDetails() {
+	inputChan := make(chan string, len(data.ParentGuids)+GrQl_Parallel)
+	outputChan := make(chan Widget, len(data.DashboardMap)+GrQl_Parallel)
+
+	// Load conditions into channel
+	go func() {
+		for _, guid := range data.ParentGuids {
+			inputChan <- guid
+		}
+		for n := 0; n < GrQl_Parallel; n++ {
+			inputChan <- ""
+		}
+	}()
+
+	log.Printf("GraphQL - starting %d dashboard detail requestors", GrQl_Parallel)
+	for i := 1; i <= GrQl_Parallel; i++ {
+		go func() {
+			var gQuery GraphQlPayload
+			var j []byte
+			var err error
+			gQuery.Query = DetailQuery
+			client := &http.Client{}
+			for {
+				guid := <-inputChan
+				if len(guid) == 0 {
+					outputChan <- Widget{}
+					break
+				}
+				gQuery.Variables.Guid = guid
+				// make query payload
+				j, err = json.Marshal(gQuery)
+				if err != nil {
+					log.Printf("Error creating dashboard detail query: %v", err)
+					continue
+				}
+				b := retryQuery(client, "POST", GraphQlEndpoint, string(j), data.GraphQlHeaders)
+				// parse results
+				var graphQlResult GraphQlResult
+				err = json.Unmarshal(b, &graphQlResult)
+				if err != nil {
+					log.Printf("Error parsing dashboard detail result: %v", err)
+					continue
+				}
+				if len(graphQlResult.Errors) > 0 {
+					if graphQlResult.Errors[0].Message == "Not Found" {
+						continue
+					}
+					log.Printf("Errors with dashboard detail query: %v", graphQlResult.Errors)
+					continue
+				}
+
+				// Cleanup widgets and send to output channel
+				for _, page := range graphQlResult.Data.Actor.Entity.Pages {
+					log.Printf("Found dashboard page %s", page.Guid)
+					for _, raw := range page.RawWidgets {
+						widget := parseWidget(raw)
+						if len(widget.NrqlQueries) == 0 {
+							// Skip if markdown or no queries
+							continue
+						}
+						widget.Guid = page.Guid
+						outputChan <- widget
+					}
+				}
+			}
+		}()
+	}
+
+	widgets := 0
+	for i := 0; i < GrQl_Parallel; i++ {
+		for {
+			output := <-outputChan
+			if len(output.Guid) == 0 {
+				break
+			}
+			dashboard, ok := data.DashboardMap[output.Guid]
+			if !ok {
+				log.Printf("Error with dashboard detail, no match for guid %d", output.Guid)
+				continue
+			}
+			dashboard.WidgetMap[output.Id] = output
+			dashboard.WidgetIds = append(dashboard.WidgetIds, output.Id)
+			data.DashboardMap[output.Guid] = dashboard
+			widgets++
+		}
+	}
+	log.Printf("GraphQL - finished dashboard detail requesters, %d widgets found", widgets)
+	for _, dashboard := range data.DashboardMap {
+		sort.Ints(dashboard.WidgetIds)
+		data.DashboardMap[dashboard.Guid] = dashboard
+	}
 }
 
 func (data *LocalData) makeClient() {
